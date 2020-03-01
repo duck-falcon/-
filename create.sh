2c5f13bac2aeb3bb7585854c00d41d723db461f9
duck-falcon <midoriiro1803@gmail.com>
2020-03-02 08:59:39 +0900

    cpp生成器作成

---
+++ create.sh
@@ -0,0 +1,9 @@
+# hinagata.cppをコピーする
+# コンパイル用シェルスクリプトも生成
+mkdir ../$1
+cp hinagata.cpp ../$1/a.cpp
+cp hinagata.cpp ../$1/b.cpp
+cp hinagata.cpp ../$1/c.cpp
+cp hinagata.cpp ../$1/d.cpp
+cp hinagata.cpp ../$1/e.cpp
+cp hinagata.cpp ../$1/f.cpp
