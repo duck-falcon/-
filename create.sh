497422f1c5d35e1d7097b1a8a0b3ffddbb00eda8
duck-falcon <midoriiro1803@gmail.com>
2020-03-25 09:39:13 +0900

    create.shを問題数の数を選択可能にする

--- create.sh
+++ create.sh
@@ -1,9 +1,12 @@
 # hinagata.cppをコピーする
 # コンパイル用シェルスクリプトも生成
+# 第二引数に問題数をとる（デフォルトは６）
+s=abcdefghijklmnopqrstuvwxyz
+d=6
+if [[ $# == 2 ]]; then
+d=$2
+fi
 mkdir ../$1
-cp hinagata.cpp ../$1/a.cpp
-cp hinagata.cpp ../$1/b.cpp
-cp hinagata.cpp ../$1/c.cpp
-cp hinagata.cpp ../$1/d.cpp
-cp hinagata.cpp ../$1/e.cpp
-cp hinagata.cpp ../$1/f.cpp
+for ((i=0 ; i<d; i++)) do
+cp hinagata.cpp ../$1/${s:${i}:1}.cpp
+done
